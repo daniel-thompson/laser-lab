269da5aaaecd98a5cd8d841374376ed728787c15
Daniel Thompson <daniel@redfelineninja.org.uk>
2018-05-28 07:51:39 +0100

    dowel: Fix length of the horizontal

--- parts/dowel_4mm.py
+++ parts/dowel_4mm.py
@@ -27,11 +27,12 @@ d = panel('dowel_4mm.svg', l, n*W)
 
 W = w + cut_width_top
 for y in range(1, n):
-    d.add(d.line((force_cut_order, W*y), (l-force_cut_order, W*y), **cut))
+    d.add(d.line((force_cut_order, W*y), (l-2*force_cut_order, W*y), **cut))
 d.add(d.rect((0, 0), (l, n*W), **cut))
 
 W = w - cut_width_top
 T = cut_width_top - cut_width_bottom
+
 d.add(rotate(translate(d.trapez((0, 0), (W, W), T/2, T/2, **cut), -2*w, -2*w), 45))
 
 d.save()
